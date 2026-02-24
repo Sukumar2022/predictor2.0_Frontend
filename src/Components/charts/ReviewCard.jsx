a81b9ea007b08f4725910c9c5d50960960a27cd3
Sukumar Mondal <104411181+Sukumar2022@users.noreply.github.com>
2026-02-24 21:46:19 +0530

    Refactor ReviewCard styles and update reviews
    Updated the ReviewCard component styles and added new review texts.

--- src/Components/charts/ReviewCard.jsx
+++ src/Components/charts/ReviewCard.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const ReviewCard = () => {
   return (
-    <div className='bg-white rounded-lg shadow-md p-6'>
-        <h3 className='pop-font bg-blue-400 text-white p-2 rounded capitalize'>Some reviews</h3>
+    <div className='bg-[#303952] rounded-lg shadow-md p-6'>
+        <h3 className='pop-font text-lg text-white p-2 rounded capitalize'>Some reviews</h3>
         <div className='flex flex-wrap gap-4 mt-4'>
             <div className='r-card w-100 lg:w-auto p-2 rounded-md transition duration-300'>
                 <p className='text-white'>"Great product! Highly recommend."</p>
             </div>
             <div className='r-card w-100 lg:w-auto p-2 rounded-md transition duration-300'>
-                <p className='text-white'>"Great product! Highly recommend."</p>
+                <p className='text-white'>"Best product, value for money"</p>
             </div>
             <div className='r-card w-100 lg:w-auto p-2 rounded-md transition duration-300'>
-                <p className='text-white'>"Great product! Highly recommend."</p>
+                <p className='text-white'>"Nice one"</p>
             </div>
         </div>
     </div>
